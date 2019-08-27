1556e7cf0db8a13af916f57c6c027b617a065982
Francisco Maciá Pérez <pmacia@MacBook-de-Francisco.local>
2019-08-28 00:40:59 +0200

    Plantilla vacía

---
+++ creator.sh
@@ -0,0 +1,20 @@
+  544  pwd
+  545  pwd
+  546  ls -la
+  547  ng g c login
+  548  ls 
+  549  ls src/app/login/
+  550  rm src/app/login/login.component.css 
+  551  rm src/app/login/login.component.spec.ts 
+  552  ng g c nopagefound --spec=false -is
+  553  ng g c pages/dashboard --skipTest -is
+  554  ng g c pages/dashboard -skipTest -is
+  555  ng g c pages/dashboard -progress -is
+  556  ng g c pages/progress -skipTest -is
+  557  ng g c pages/graficas1 -skipTest -is
+  558  ng g c shared/header -skipTest -is
+  559  ng g c shared/sidebar -skipTest -is
+  560  ng g c shared/breadcrumbs -skipTest -is
+  561  history
+  562  history | tail -20
+  563  history | tail -20 > kk.sh
