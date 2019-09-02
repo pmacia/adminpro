0fcedb7318507e457e2f5f210dade20e2d280381
Francisco Maciá Pérez <pmacia@MacBook-de-Francisco.local>
2019-09-02 11:12:28 +0200

    primeras promesas y observables. actualización con history

---
+++ historico-adminpro-v1.2.sh
@@ -0,0 +1,501 @@
+  170  cd servidorHTTP/
+  171  ls -la
+  172  rm miHTTPd 
+  173  clear && gcc main.c protocolo.c -o miHTTPd
+  174  ./miHTTPd 
+  175  clear && gcc main.c protocolo.c -o miHTTPd -D DEBUG
+  176  ./miHTTPd 
+  177  clear && gcc main.c protocolo.c -o miHTTPd -D DEBUG
+  178  ./miHTTPd 
+  179  ps -ef | grep miHTTP
+  180  exit
+  181  ifconfig
+  182  netstat -rn
+  183  ping 192.168.2.15
+  184  ifconfig
+  185  ping 172.27.45.29
+  186  pwd
+  187  exit
+  188  clear && ./miHTTP2
+  189  clear && gcc main_bateria_pruebas.c -o miHTTP2 -D DEBUG
+  190  clear && ./miHTTP2
+  191  clear && ./miHTTP2
+  192  clear && ./miHTTP2
+  193  clear && ./miHTTP2
+  194  clear && ./miHTTP2
+  195  clear && ./miHTTP2
+  196  ps -ef | grep miHTT
+  197  ps -ef | grep miHTT
+  198  clear && gcc main_bateria_pruebas.c -o miHTTP2 -D DEBUG
+  199  clear && ./miHTTP2
+  200  clear && ./miHTTP2
+  201  clear && ./miHTTP2
+  202  clear && ./miHTTP2
+  203  clear && ./miHTTP2
+  204  ps -ef | grep miHTT
+  205  ps -ef | grep miHTT
+  206  clear && ./miHTTP2
+  207  clear && ./miHTTP2
+  208  clear && ./miHTTP2
+  209  clear && ./miHTTP2
+  210  clear && ./miHTTP2
+  211  clear && ./miHTTP2
+  212  clear && ./miHTTP2
+  213  clear && ./miHTTP2
+  214  ps -ef | grep miHTT
+  215  ps -ef | grep miHTT
+  216  clear && ./miHTTP2
+  217  clear && ./miHTTP2
+  218  clear && ./miHTTP2
+  219  clear && ./miHTTP2
+  220  clear && ./miHTTP2
+  221  clear && gcc main_bateria_pruebas.c -o miHTTP2 -D DEBUG
+  222  clear && ./miHTTP2
+  223  clear && ./miHTTP2
+  224  clear && ./miHTTP2
+  225  clear && ./miHTTP2
+  226  clear && ./miHTTP2
+  227  clear && ./miHTTP2
+  228  clear && ./miHTTP2
+  229  clear && ./miHTTP2
+  230  clear && ./miHTTP2
+  231  clear && ./miHTTP2
+  232  clear && ./miHTTP2
+  233  clear && ./miHTTP2
+  234  clear && ./miHTTP2
+  235  clear && ./miHTTP2
+  236  clear && ./miHTTP2
+  237  clear && ./miHTTP2
+  238  clear && ./miHTTP2
+  239  clear && ./miHTTP2
+  240  clear && ./miHTTP2
+  241  clear && ./miHTTP2
+  242  clear && ./miHTTP2
+  243  clear && ./miHTTP2
+  244  clear && ./miHTTP2
+  245  clear && ./miHTTP2
+  246  clear && ./miHTTP2
+  247  clear && ./miHTTP2
+  248  clear && ./miHTTP2
+  249  clear && ./miHTTP2
+  250  clear && ./miHTTP2
+  251  clear && ./miHTTP2
+  252  clear && ./miHTTP2
+  253  clear && ./miHTTP2
+  254  clear && ./miHTTP2
+  255  clear && ./miHTTP2
+  256  clear && ./miHTTP3 http://localhost:8080/
+  257  clear && ./miHTTP3 http://localhost:8080/logo.png
+  258  clear && ./miHTTP3 http://localhost:8080/logo.png
+  259  clear && ./miHTTP3 http://localhost:8080/logo.png
+  260  clear && ./miHTTP3 http://localhost:8080/logo.png
+  261  clear && ./miHTTP3 http://localhost:8080/logo.png
+  262  clear && ./miHTTP3 http://localhost:8080/logo.png
+  263  clear && ./miHTTP3 http://localhost:8080/logo.png
+  264  clear && ./miHTTP3 http://localhost:8080/logo
+  265  clear && ./miHTTP3 http://localhost:8080/logo
+  266  clear && ./miHTTP3 http://localhost:8080/logo.png
+  267  clear && ./miHTTP3 http://localhost:8080/logo.png
+  268  clear && ./miHTTP3 http://localhost:8080/
+  269  clear && ./miHTTP3 http://localhost:8080/logo.png
+  270  clear && ./miHTTP3 http://localhost:8080/logo.png
+  271  clear && ./miHTTP3 http://localhost:8080/
+  272  clear && ./miHTTP3 http://localhost:8080/
+  273  clear && ./miHTTP3 http://localhost:8080/
+  274  clear && ./miHTTP3 http://localhost:8080/
+  275  clear && ./miHTTP3 http://localhost:8080/
+  276  clear && ./miHTTP3 http://localhost:8080/
+  277  clear && ./miHTTP3 http://localhost:8080/
+  278  clear && ./miHTTP3 http://localhost:8080/
+  279  clear && ./miHTTP3 http://localhost:8080/
+  280  clear && ./miHTTP3 http://localhost:8080/
+  281  clear && ./miHTTP3 http://localhost:8080/
+  282  clear && ./miHTTP3 http://localhost:8080/
+  283  clear && ./miHTTP3 http://localhost:8080/
+  284  clear && ./miHTTP3 http://localhost:8080/
+  285  clear && ./miHTTP3 http://localhost:8080/
+  286  ./miHTTP2
+  287  ./miHTTP2
+  288  ./miHTTP2
+  289  ./miHTTP2
+  290  ./miHTTP2
+  291  ./miHTTP2
+  292  ./miHTTP2
+  293  ./miHTTP2
+  294  ./miHTTP2
+  295  ./miHTTP2
+  296  ./miHTTP2
+  297  ./miHTTP2
+  298  ./miHTTP2
+  299  ./miHTTP2
+  300  ./miHTTP2
+  301  ./miHTTP2
+  302  clear && gcc main_bateria_pruebas.c -o miHTTP2 -D DEBUG
+  303  ./miHTTP2
+  304  ./miHTTP2
+  305  ./miHTTP2
+  306  ./miHTTP2
+  307  ./miHTTP2
+  308  ./miHTTP2
+  309  ./miHTTP2
+  310  ./miHTTP2
+  311  ./miHTTP2
+  312  ./miHTTP2
+  313  ./miHTTP2
+  314  ./miHTTP2
+  315  ./miHTTP2
+  316  ./miHTTP2
+  317  ./miHTTP2
+  318  ./miHTTP2
+  319  ./miHTTP
+  320  ./miHTTP http localhost 8080 /
+  321  ./miHTTP http localhost 8080 /
+  322  ./miHTTP http localhost 8080 /
+  323  ./miHTTP http localhost 8080 /
+  324  ./miHTTP http localhost 8080 /
+  325  ./miHTTP http localhost 8080 /
+  326  ./miHTTP2
+  327  ./miHTTP2
+  328  ./miHTTP2
+  329  ./miHTTP2
+  330  ./miHTTP3
+  331  ./miHTTP3 http://localhost:8080/index.html
+  332  clear && gcc main_bateria_pruebas.c -o miHTTP2 -D DEBUG
+  333  clear && gcc main_bateria_pruebas.c -o miHTTP2 -D DEBUG
+  334  ./miHTTP2
+  335  clear && gcc main_bateria_pruebas.c -o miHTTP2 -D DEBUG
+  336  ./miHTTP2
+  337  clear && gcc main_bateria_pruebas.c -o miHTTP2 -D DEBUG
+  338  ./miHTTP2
+  339  ./miHTTP2
+  340  clear && gcc main_bateria_pruebas2.c -o miHTTP4 -D DEBUG
+  341  clear && gcc main_bateria_pruebas2.c -o miHTTP4 -D DEBUG
+  342  ./miHTTP4
+  343  ping www.dtic.ua.es
+  344  clear && gcc main_bateria_pruebas2.c -o miHTTP4 -D DEBUG
+  345  ./miHTTP4
+  346  clear && gcc main_bateria_pruebas3.c -o miHTTP5 -D DEBUG
+  347  clear && gcc main_bateria_pruebas3.c -o miHTTP5 -D DEBUG
+  348  clear && gcc main_bateria_pruebas3.c -o miHTTP5 -D DEBUG
+  349  ./miHTTP5
+  350  ./miHTTP5
+  351  clear && gcc main_bateria_pruebas3.c -o miHTTP5 -D DEBUG
+  352  ./miHTTP5
+  353  ./miHTTP5
+  354  clear && gcc main_bateria_pruebas3.c -o miHTTP5 -D DEBUG
+  355  ./miHTTP5
+  356  clear && gcc main_bateria_pruebas3.c -o miHTTP5 -D DEBUG
+  357  ./miHTTP5
+  358  clear && gcc main_bateria_pruebas3.c -o miHTTP5 -D DEBUG
+  359  ./miHTTP5
+  360  ./miHTTP2
+  361  ./miHTTP3
+  362  ./miHTTP4
+  363  clear && gcc main_bateria_pruebas3.c -o miHTTP3 -D DEBUG
+  364  clear && gcc main_bateria_pruebas2.c -o miHTTP2 -D DEBUG
+  365  ./miHTTP2
+  366  ./miHTTP2
+  367  ./miHTTP3
+  368  clear
+  369  ./miHTTP3
+  370  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  371  ./miHTTP
+  372  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  373  ./miHTTP
+  374  ./miHTTP
+  375  ./miHTTP
+  376  ./miHTTP
+  377  ./miHTTP
+  378  clear
+  379  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  380  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  381  ./miHTTP
+  382  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  383  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  384  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  385  ./miHTTP
+  386  ./miHTTP 2> /dev/null
+  387  clear
+  388  ./miHTTP 2> /dev/null
+  389  clear
+  390  clear
+  391  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  392  ./miHTTP 2> /dev/null
+  393  clear && ./miHTTP
+  394  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  395  clear && ./miHTTP
+  396  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  397  clear && ./miHTTP
+  398  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  399  clear && ./miHTTP
+  400  clear && gcc main_bateria_pruebas2.c -o miHTTP -D DEBUG
+  401  clear && ./miHTTP
+  402  clear && ./miHTTP
+  403  clear && ./miHTTP
+  404  clear && ./miHTTP & ./miHTTP & ./miHTTP
+  405  pwd
+  406  cd ..
+  407  cd servidorHTTP/
+  408  ls
+  409  ./inicia_miHTTPd.sh 
+  410  ./crea_miHTTPd.sh 
+  411  ./i
+  412  ./inicia_miHTTPd.sh 
+  413  ./inicia_miHTTPd.sh 
+  414  ./inicia_miHTTPd.sh 
+  415  ./crea_miHTTPd.sh 
+  416  ./inicia_miHTTPd.sh 
+  417  ./inicia_miHTTPd.sh 
+  418  pwd
+  419  ls -la
+  420  pwd
+  421  ls
+  422  cd Documents/
+  423  ls
+  424  ls JS
+  425  ls -la
+  426  pwd
+  427  cd ..
+  428  ls
+  429  cd OneDrive
+  430  ls
+  431  cd SD\ 2018-19/
+  432  ls
+  433  cd Prácticas/
+  434  ls
+  435  cd P03
+  436  ls
+  437  cd RESTFulServer/
+  438  cd ..
+  439  mkdir KKP2
+  440  cd KKP2/
+  441  npm init
+  442  node
+  443  node
+  444  whereis node
+  445  whereis npm
+  446  ls
+  447  pwd
+  448  cd ..
+  449  ls
+  450  cd RESTFulServer/
+  451  ls
+  452  node
+  453  npm
+  454  vscode
+  455  ls /opt
+  456  pwd
+  457  ls
+  458  cd Dropbox\ \(Universidad\ de\ Alicante\)/
+  459  ls
+  460  cd TMP/
+  461  cd SD/
+  462  ls
+  463  cd Tutoría/
+  464  ls
+  465  cd tamara/
+  466  ls
+  467  la -la
+  468  ls -la
+  469  gcc main.c protocolo.c -D DEBUG
+  470  httpd
+  471  gcc main.c protocolo.c -D DEBUG
+  472  httpd
+  473  ./miHTTPd
+  474  ls
+  475  ls -la
+  476  ./a.out 
+  477  cat httpd.conf 
+  478  gcc main.c protocolo.c -o miHTTPd -D DEBUG
+  479  ./miHTTPd 
+  480  ./miHTTPd 
+  481  gcc main.c protocolo.c -o miHTTPd -D DEBUG
+  482  ./miHTTPd 
+  483  cat httpd.conf 
+  484  exit
+  485  cd
+  486  ls -la
+  487  cd Library/
+  488  ls -la
+  489  cd Application\ Support/
+  490  ls
+  491  ls -la
+  492  cd cura/
+  493  ls
+  494  cd 4.0/
+  495  ls
+  496  ls -la
+  497  pwd
+  498  exit
+  499  code
+  500  exit
+  501  ls -la
+  502  pwd
+  503  mkdir desarrollo
+  504  cd desaroolo
+  505  cd desarrollo
+  506  ls -la
+  507  ng
+  508  ls -la
+  509  code
+  510  whereis code
+  511  whereis vscode
+  512  node
+  513  node v
+  514  node -v
+  515  npm v
+  516  npm -v
+  517  npm install -g @angular/cli
+  518  ng -v
+  519  sudo npm install -g @angular/cli
+  520  ng -v
+  521  ng v
+  522  ls -la
+  523  pwd
+  524  ls -la
+  525  mkdir angular
+  526  cd angular/
+  527  ng n hello
+  528  ls -la
+  529  cd hello
+  530  ng s -0
+  531  ng s -o
+  532  ng --version
+  533  ng v
+  534  ls -la
+  535  ls -la
+  536  cd ..
+  537  pwd
+  538  ng n adminpro
+  539  ls -a
+  540  cd adminpro/
+  541  ls -la
+  542  ls -la
+  543  ng s 
+  544  pwd
+  545  pwd
+  546  ls -la
+  547  ng g c login
+  548  ls 
+  549  ls src/app/login/
+  550  rm src/app/login/login.component.css 
+  551  rm src/app/login/login.component.spec.ts 
+  552  ng g c nopagefound --spec=false -is
+  553  ng g c pages/dashboard --skipTest -is
+  554  ng g c pages/dashboard -skipTest -is
+  555  ng g c pages/dashboard -progress -is
+  556  ng g c pages/progress -skipTest -is
+  557  ng g c pages/graficas1 -skipTest -is
+  558  ng g c shared/header -skipTest -is
+  559  ng g c shared/sidebar -skipTest -is
+  560  ng g c shared/breadcrumbs -skipTest -is
+  561  history
+  562  history | tail -20
+  563  history | tail -20 > kk.sh
+  564  pwd
+  565  mv kk.sh creator.sh
+  566  history | tail -50 > kk.sh
+  567  ng g s services/shared -skipTest
+  568  ng g s services/shared -skipTest
+  569  ng g s services/shared --spec=false
+  570  ng g s services/sidebar -skipTests
+  571  ng g s services/sidebar -skipTests
+  572  ng g s services/sidebar --spec=false
+  573  npm i -S jquery popper.js font-awesome bootstrap
+  574  ng s
+  575  ng s
+  576  ng g c pages/pages --flat -is --spec=false
+  577  ng g c login/register --flat -is --spec=false
+  578  pwd
+  579  git
+  580  git status
+  581  git init
+  582  git add .
+  583  git status
+  584  git commit -m "Plantilla vacía"
+  585  git remote add origin https://github.com/pmacia/adminpro.git
+  586  git push -u origin master
+  587  git status
+  588  git commit -am "Rutas en módulos"
+  589  git add
+  590  git status
+  591  gid add
+  592  git add
+  593  git status
+  594  git add .
+  595  git status
+  596  git commit -am "Rutas en módulos"
+  597  git push
+  598  git tag -a v1.0.0 -m "Versión 1 - Esqueleto funcional vacío listo para producción"
+  599  git push --tgas
+  600  git push --tags
+  601  git status
+  602  git add .
+  603  git commit -m "Versión 1 con bugs corregidos: wrapper y router-outlet
+"
+  604  git push
+  605  git tag -a v1.0.1 -m "Versión 1 - Esqueleto funcional vacío listo para producción"
+  606  git push --tags
+  607  history
+  608  history | tail -110
+  609  history | tail -108 > historico-v1.sh
+  610  git status
+  611  more *.sh
+  612  more *.sh
+  613  history | tail -150 > historico-v1.sh
+  614  more *.sh
+  615  history | tail -n 120 > historico-v1.sh
+  616  more *.sh
+  617  rm historico-v1.sh 
+  618  history | tail -n 120
+  619  history | tail -n 120 > historico-adminpro-v1.sh
+  620  cat historico-adminpro-v1.sh 
+  621  ng g c components/incrementador --is --spec=false
+  622  ng g c components/incrementador -is --spec=false
+  623  npm i -S ng2-charts chart.js
+  624  ng g c components/graficaDonut -is --spec=false
+  625  git status
+  626  git add .
+  627  git commint -m "Primera versión funcional con dos módulos: progress y doughtnut"
+  628  git commit -m "Primera versión funcional con dos módulos: progress y doughtnut"
+  629  git push
+  630  git tag
+  631  git tag v1.1.0
+  632  git tag
+  633  git tag -a v1.1.0 -m "Versión 2 - Módulos incremento y gráficas de donut"
+  634  git push --tags
+  635  ng g c pages/accountSettings --module="pages/pages.module.ts" --spec=false -is
+  636  ng g s services/settings -m="app.module.ts" --spec=false
+  637  ng g s services/settings --module="app.module.ts" --spec=false
+  638  ng g s services/settings --spec=false
+  639  ng g m services/service --spec=false --flat
+  640  ng g m services/service --flat
+  641  git status
+  642  git add .
+  643  git commit -m "Añadida la configuración de temas (colores) y el módulo de indexación de servicios"
+  644  git push
+  645  git status
+  646  ls -la
+  647  cp angular.json angular.json.old
+  648  git status
+  649  git add .
+  650  git commit -m "Actualización con bootstrap y compañía fijados en assets/plugins"
+  651  git push
+  652  git status
+  653  git add .
+  654  git status
+  655  git commit -m "Actualización con login y carga controlado de los plugins"
+  656  git push
+  657  git tag
+  658  git tag -a v1.2.0 -m "Fin sección 7 del curso Udemy"
+  659  git tag
+  660  git push --tags
+  661  ng g c pages/promesas --spec=false -is
+  662  ng g c pages/rxjs --spec=false -is
+  663  git status
+  664  git add .
+  665  git commit -m "primeras promesas y observables"
+  666  git push
+  667  history
+  668  ls *.sh
+  669  history > historico-adminpro-v1.2.sh
